ac497dde6549e41b3e1604f8f8c81e29e447f83a
rasamkamyar <rasamkamyar@gmail.com>
2024-05-17 18:04:28 +0330

    handle play game progress

--- src/features/Play.tsx
+++ src/features/Play.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { QuizItem } from "../types/quiz-type";
 import {
   Flex,
@@ -8,14 +8,35 @@ import {
   SimpleGrid,
   Text,
 } from "@chakra-ui/react";
+import Lottie from "lottie-react";
+import validAnime from "../assets/lottie/valid.json";
+import inValidAnime from "../assets/lottie/invalid.json";
 
 function Play(p: { quiz: QuizItem[] }) {
   const [currentQuizItemIndex, setCurrentQuizItemIndex] = useState<number>(0);
+  const [answer, setAnswer] = useState<string>();
+  const [questionStatus, setQuestionStatus] = useState<
+    "valid" | "invalid" | "unaswered"
+  >("unaswered");
   const currentQuizItem: QuizItem = p.quiz[currentQuizItemIndex];
   const avalaibleAnswers: string[] = [
     currentQuizItem.correct_answer,
     ...currentQuizItem.incorrect_answers,
   ];
+
+  useEffect(() => {
+    if (answer) {
+      if (isValidAnswer(answer)) {
+        setQuestionStatus("valid");
+      } else {
+        setQuestionStatus("invalid");
+      }
+    }
+  }, [answer]);
+
+  const isValidAnswer = (answer: string): boolean => {
+    return answer === currentQuizItem.correct_answer;
+  };
   const radioList = avalaibleAnswers.map((avalaibleAnswer: string) => {
     return (
       <Radio key={avalaibleAnswer} value={avalaibleAnswer}>
@@ -33,12 +54,24 @@ function Play(p: { quiz: QuizItem[] }) {
         mb={10}
         dangerouslySetInnerHTML={{ __html: currentQuizItem.question }}
       />
-      <RadioGroup
-        value=""
-        onChange={() => setCurrentQuizItemIndex(currentQuizItemIndex + 1)}
-      >
+      <RadioGroup value={answer} onChange={setAnswer}>
         <SimpleGrid>{radioList}</SimpleGrid>
       </RadioGroup>
+      <Lottie
+        loop={false}
+        style={{ marginTop: "20px", height: "200px" }}
+        animationData={
+          questionStatus === "unaswered"
+            ? null
+            : questionStatus === "valid"
+            ? validAnime
+            : inValidAnime
+        }
+        onComplete={() => {
+          setQuestionStatus("unaswered");
+          setCurrentQuizItemIndex(currentQuizItemIndex + 1);
+        }}
+      />
     </Flex>
   );
 }
