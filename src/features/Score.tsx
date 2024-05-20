0c4788fa77bcdb17d0c610be09b65c99318346bc
rasamkamyar <rasamkamyar@gmail.com>
2024-05-20 13:09:46 +0330

    finish project

--- src/features/Score.tsx
+++ src/features/Score.tsx
@@ -1,16 +1,43 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Button, Flex, Heading, Text } from "@chakra-ui/react";
 
-function Score(p: { history: boolean[] }) {
+function Score(p: { history: boolean[]; onNext: () => void }) {
   const rightAnswers = p.history.filter(
     (isValidAnswer: boolean) => isValidAnswer === true
   ).length;
-  console.log(history.length);
+
+  const renderMessage = () => {
+    const rightAnswerPercentage = (rightAnswers * 100) / p.history.length;
+    if (rightAnswerPercentage < 30) {
+      return <Heading colorScheme="red">You need more practice</Heading>;
+    } else if (rightAnswerPercentage < 50) {
+      return <Heading colorScheme="red">Not bad,Try more</Heading>;
+    } else if (rightAnswerPercentage < 70) {
+      return <Heading colorScheme="red">Good job</Heading>;
+    } else {
+      return <Heading colorScheme="red">Fantastic</Heading>;
+    }
+  };
   return (
-    <Flex>
+    <Flex
+      direction={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      mt={"20px"}
+      gap={"50px"}
+    >
       <Heading>Score</Heading>
       <Heading>
         {rightAnswers}/{p.history.length}
       </Heading>
+      <Text fontSize={"xl"}>{renderMessage()}</Text>
+      <Button
+        onClick={p.onNext}
+        position={"absolute"}
+        right={"10%"}
+        top={"80%"}
+      >
+        New Game
+      </Button>
     </Flex>
   );
 }
